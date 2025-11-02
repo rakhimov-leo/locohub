fe173dbf37f11db2b19265f214377cb6b58fbb6c
Rakhimov Navruz <alina@Rakhimovs-MacBook-Pro.local>
2025-11-02 21:16:44 +0900

    feat: fixed errors

--- apps/nestar-api/src/libs/dto/member/member.update.ts
+++ apps/nestar-api/src/libs/dto/member/member.update.ts
@@ -5,40 +5,43 @@ import { ObjectId } from 'mongoose';
 
 @InputType()
 export class MemberUpdate {
+	@IsNotEmpty()
+	@Field(() => String)
+	_id: ObjectId;
 
-    @IsNotEmpty()
-    @Field(() => String)
-    _id: ObjectId
+	@IsOptional()
+	@Field(() => MemberType, { nullable: true })
+	memberType?: MemberType;
 
-    @IsOptional()
-    @Field(() => MemberType, { nullable: true })
-    memberType?: MemberType;
+	@IsOptional()
+	@Field(() => MemberStatus, { nullable: true })
+	memberStatus?: MemberStatus;
 
-    @IsOptional()
-    @Field(() => MemberStatus, { nullable: true })
-    memberStatus?: MemberStatus;
+	@IsOptional()
+	@Field(() => String, { nullable: true })
+	memberPhone?: string;
 
-    @IsOptional()
-    @Field(() => String, { nullable: true })
-    memberPhone?: string;
+	@IsOptional()
+	@Length(3, 12)
+	@Field(() => String, { nullable: true })
+	memberNick?: string;
 
-    @IsOptional()
-    @Length(3, 12)
-    @Field(() => String, { nullable: true })
-    memberNick?: string;
+	@IsOptional()
+	@Length(5, 12)
+	@Field(() => String, { nullable: true })
+	memberPassword?: string;
 
-    @IsOptional()
-    @Length(5, 12)
-    @Field(() => String, { nullable: true })
-    memberPassword?: string;
+	@IsOptional()
+	@Length(3, 100)
+	@Field(() => String, { nullable: true })
+	memberFullName?: string;
 
-    @IsOptional()
-    @Length(3, 100)
-    @Field(() => String, { nullable: true })
-    memberFullName?: string;
+	@IsOptional()
+	@Field(() => String, { nullable: true })
+	memberImage?: string;
 
-    @IsOptional()
-    @Field(() => String, { nullable: true })
-    memberImage?: string;
-
+	@IsOptional()
+	@Length(3, 100)
+	@Field(() => String, { nullable: true })
+	memberAddress?: string; // 👈 yangi qo‘shildi
 }
