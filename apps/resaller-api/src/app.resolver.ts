71cdae83a48449dfa3658e9942e48986054430f1
Rakhimov Navruz <alina@Rakhimovs-MacBook-Pro.local>
2025-12-10 03:00:11 +0900

    feat: changed project name and modify some files

--- apps/resaller-api/src/app.resolver.ts
+++
@@ -1,9 +0,0 @@
-import { Query, Resolver } from '@nestjs/graphql';
-
-@Resolver()
-export class AppResolver {
-	@Query(() => String)
-	public sayHello(): string {
-		return 'GRAPHQL API server';
-	}
-}
